84d52f9b51981215babe9e157b3037243cfb739b
Whitney Hunter <whitney@aiontechnology.io>
2020-05-08 17:45:21 -0700

    MS-43 Added ability to add a teacher to a school

---
+++ src/app/schoolmanager/directives/phone-format.directive.ts
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2020 Aion Technology LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Directive, HostListener } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+@Directive({
+  selector: '[msPhoneFormat]'
+})
+export class PhoneFormatDirective {
+
+  constructor(public ngControl: NgControl) { }
+
+  @HostListener('ngModelChange', ['$event'])
+  onModelChange(event) {
+    this.onInputChange(event, false);
+  }
+
+  @HostListener('keydown.backspace', ['$event'])
+  keydownBackspace(event) {
+    this.onInputChange(event.target.value, true);
+  }
+
+  onInputChange(event, backspace) {
+    let newVal = event.replace(/\D/g, '');
+    if (backspace && newVal.length <= 6) {
+      newVal = newVal.substring(0, newVal.length - 1);
+    }
+    if (newVal.length === 0) {
+      newVal = '';
+    } else if (newVal.length <= 3) {
+      newVal = newVal.replace(/^(\d{0,3})/, '($1)');
+    } else if (newVal.length <= 6) {
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, '($1) $2');
+    } else if (newVal.length <= 10) {
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
+    } else {
+      newVal = newVal.substring(0, 10);
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
+    }
+    this.ngControl.valueAccessor.writeValue(newVal);
+  }
+
+}
