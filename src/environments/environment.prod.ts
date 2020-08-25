239cea045b2aaaae83db4d4c72d5dd48e700530f
Whitney Hunter <whitney@aiontechnology.io>
2020-08-25 14:30:14 -0700

    MS-96 Put in some debug code

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-
-export const environment = {
-   production: true,
-   tokenRedirect: "http://localhost:4200/receiveToken",
-   logoutRedirect: "http://localhost:4200/handleLogout",
-   apiUri: "http://localhost:4200/handleLogout",
-   cognitoClientId: "76on3r6c055h1pp2h99uc6jmbd",
-   cognitoBaseUrl: "mentorsuccess-localhost-1.auth.us-west-2.amazoncognito.com"
-};
