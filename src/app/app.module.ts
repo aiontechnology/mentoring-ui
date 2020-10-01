4dba2c7de7d2aca6b7ab775b694ead57637dbcd3
Whitney Hunter <whitney@aiontechnology.io>
2020-10-01 08:50:15 -0700

    MS-15 Added module and component for student-manager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,10 +42,12 @@ const routes: Routes = [
   { path: 'logout', component: NoopComponent, canActivate: [logoutProvider] },
   { path: 'receiveToken', component: ReceiveTokenComponent },
   { path: 'handleLogout', component: HandleLogoutComponent },
+  { path: 'resourcemanager', loadChildren: () => import('./modules/resource-manager/resource-manager.module')
+    .then(m => m.ResourceManagerModule) },
   { path: 'schoolsmanager', loadChildren: () => import('./modules/school-manager/school-manager.module')
     .then(m => m.SchoolManagerModule) },
-  { path: 'resourcemanager', loadChildren: () => import('./modules/resource-manager/resource-manager.module')
-    .then(m => m.ResourceManagerModule) }
+  { path: 'studentmanager', loadChildren: () => import('./modules/student-manager/student-manager.module')
+    .then(m => m.StudentManagerModule) }
 ];
 
 @NgModule({
