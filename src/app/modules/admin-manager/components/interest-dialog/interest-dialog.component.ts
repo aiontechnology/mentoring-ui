ea65c87c9cbd48e98c311adc02172ad5e6e549ee
Hunter <hunter.purviance@icloud.com>
2021-02-11 15:53:23 -0800

    MS-253 Add validator to prevent creating duplicate interests

--- src/app/modules/admin-manager/components/interest-dialog/interest-dialog.component.ts
+++ src/app/modules/admin-manager/components/interest-dialog/interest-dialog.component.ts
@@ -14,42 +14,77 @@
  * limitations under the License.
  */
 
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { AbstractControl, FormGroup, FormBuilder, ValidatorFn } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MetaDataService } from 'src/app/modules/shared/services/meta-data/meta-data.service';
 import { InterestOutbound } from 'src/app/modules/shared/models/meta-data/interests/interest-outbound';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ms-interest-dialog',
   templateUrl: './interest-dialog.component.html',
   styleUrls: ['./interest-dialog.component.scss']
 })
-export class InterestDialogComponent {
+export class InterestDialogComponent implements OnInit, OnDestroy {
 
   private isUpdate: boolean;
   private updateValue: string; // 'CREATE_ME' for new interest, and 'old_value' for edited interest.
 
-  interestBinding: string; // Value bound to template input.
   dialogTitle: string;
+  model: FormGroup;
+
+  interestSubscription$: Subscription;
+  interests: string[];
 
   constructor(private dialogRef: MatDialogRef<InterestDialogComponent>,
               private metaDataService: MetaDataService,
+              private formBuilder: FormBuilder,
               @Inject(MAT_DIALOG_DATA) private data: any) {
 
+    const updateInterest = data?.model ? data.model.name : '';
+    this.updateValue = updateInterest ? updateInterest : 'CREATE_ME';
+
     this.isUpdate = this.determineUpdate(data);
-    this.interestBinding = data?.model ? data.model.name : '';
-    this.updateValue = this.interestBinding ? this.interestBinding : 'CREATE_ME';
 
     this.dialogTitle = this.isUpdate ? 'Edit Interest' : 'Add New Interest';
+    this.createModel(formBuilder, updateInterest);
 
+  }
+
+  ngOnInit(): void {
+    this.interestSubscription$ = this.metaDataService.interests.subscribe(data => {
+      console.log('Interest dialog component recieving meta-data interests:', data);
+      this.interests = data;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.interestSubscription$.unsubscribe();
+  }
+
+  createModel(formBuilder: FormBuilder, interest: string): void {
+    this.model = formBuilder.group({
+      name: [interest, this.duplicateInterestValidator()]
+    });
+  }
+
+  duplicateInterestValidator(): ValidatorFn {
+    const errorMsg = 'This interest already exists';
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      if (this.interests?.includes(control.value) && this.interests !== undefined) {
+        return { duplicateInterest: { msg: errorMsg } };
+      }
+      return null;
+    };
   }
 
   save(): void {
 
+    const newInterest = this.model.value.name;
     const value: InterestOutbound = {
-      [this.interestBinding]: this.updateValue
+      [newInterest]: this.updateValue
     };
-
     const ret = this.metaDataService.updateInterests(value);
 
     ret.then((i: string[]) => {
