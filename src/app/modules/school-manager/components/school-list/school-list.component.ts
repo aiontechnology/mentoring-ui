d9dd99ed98b6b6848ff50788c1cf762573ff1a23
Whitney Hunter <whitney@aiontechnology.io>
2022-10-31 20:43:08 +0000

    Merged in bugfix/MS-532 (pull request #209)
    MS-532 Fixed school list menu handling bug
    
    * MS-532 Fixed school list menu handling bug

--- src/app/modules/school-manager/components/school-list/school-list.component.ts
+++ src/app/modules/school-manager/components/school-list/school-list.component.ts
@@ -50,6 +50,7 @@ export class SchoolListComponent extends ListComponent<School> implements OnInit
   @ViewChild(MatPaginator) set paginator(paginator: MatPaginator) { super.paginator = paginator }
 
   ngOnInit(): void {
+    this.menuState.reset()
     this.init()
   }
 
